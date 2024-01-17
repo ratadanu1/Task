f8688095559d902e7368218e17d7bb0dd4ee192f
Daniel Rata <rata.daniel@usm.md>
2024-01-17 13:21:06 +0200

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        public static void CellColor(string cell)
+        {
+            System.Console.WriteLine((cell[0] + cell[1]) % 2 == 0 ? "cell is black" : "cell is white");
+        }
+
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Enter the cell: ");
+            var cell = Console.ReadLine();
+
+            CellColor(cell);
+
+        }
+    }
+}
